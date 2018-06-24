ba855b68ac3fbf5df8b94aa8ece6d8437208d5ea
Alexander Mextner <a-x-@users.noreply.github.com>
2018-06-24 10:54:41 +0300

    cover no npm case

--- noyarn.sh
+++ noyarn.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 echo
-echo We're using npm5+ with package-lock.json instead of yarn
+echo We\'re using npm5+ with package-lock.json instead of yarn
 echo
-echo Your npm version is $( npm -v )
+
+if [ "$( which npm )" ]; then
+  echo Your npm version is $( npm -v )
+else
+  echo You have not any npm installation
+fi
+
 echo
 echo Tip. Install node and npm using tj/n node version manager:
-echo curl -L https://git.io/n-install | bash
+echo "curl -L https://git.io/n-install | bash"
+echo "Update npm: npm i -g npm"
 echo
 echo
 exit 1
