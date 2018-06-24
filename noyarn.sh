9cb9740d79e570cee7b3047c705df0e996eb13c9
Alexander Mextner <a-x-@users.noreply.github.com>
2018-06-24 10:48:13 +0300

    enhance texts

--- noyarn.sh
+++ noyarn.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
-echo We use npm5+ with package-lock.json instead of yarn
+echo
+echo We're using npm5+ with package-lock.json instead of yarn
+echo
 echo Your npm version is $( npm -v )
+echo
+echo Tip. Install node and npm using tj/n node version manager:
+echo curl -L https://git.io/n-install | bash
+echo
+echo
 exit 1
